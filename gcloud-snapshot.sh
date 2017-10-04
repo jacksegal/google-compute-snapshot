f0c3fe6376d54e75b20cf72456012825fd022fa1
Jack <jackbsegal@gmail.com>
2017-10-04 20:47:04 +0100

    Trimming UTC timezone from date to fix centos bug #25

--- gcloud-snapshot.sh
+++ gcloud-snapshot.sh
@@ -184,7 +184,7 @@ getSnapshotCreatedDate()
     local snapshot_datetime="$(gcloud compute snapshots describe $1 | grep "creationTimestamp" | cut -d " " -f 2 | tr -d \')"
 
     #  format date
-    echo -e "$(date -d ${snapshot_datetime} +%Y%m%d)"
+    echo -e "$(date -d ${snapshot_datetime%?????} +%Y%m%d)"
 }
 
 
